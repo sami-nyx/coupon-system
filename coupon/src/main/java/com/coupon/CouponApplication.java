7ea71beb8e9aa065c8c3620486d535a1e80f56bd
samin <samin@DESKTOP-C5U47SK.mshome.net>
2022-09-21 23:40:23 +0300

    added models and types initializing company bean using class path xml configuration file

--- coupon/src/main/java/com/coupon/CouponApplication.java
+++ coupon/src/main/java/com/coupon/CouponApplication.java
@@ -2,12 +2,21 @@ package com.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.coupon.model.Company;
 
 @SpringBootApplication
 public class CouponApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponApplication.class, args);
+		
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+	Company company=(Company)ctx.getBean("company");
+	System.out.println(company);
+	
 	}
 
 }
